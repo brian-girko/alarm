6f032f332f2ca1243ecfb2663ce3c253203e5d3f
Brian Girko <brian.girko@gmail.com>
2019-08-29 06:15:32 +0000

    version 0.1.1

--- background.js
+++ background.js
@@ -162,3 +162,31 @@ chrome.runtime.onMessage.addListener((request, sender, respose) => {
     chrome.notifications.getAll(ns => Object.keys(ns).forEach(silent));
   }
 });
+
+// FAQs and Feedback
+{
+  const {onInstalled, setUninstallURL, getManifest} = chrome.runtime;
+  const {name, version} = getManifest();
+  const page = getManifest().homepage_url;
+  onInstalled.addListener(({reason, previousVersion}) => {
+    chrome.storage.local.get({
+      'faqs': true,
+      'last-update': 0
+    }, prefs => {
+      if (reason === 'install' || (prefs.faqs && reason === 'update')) {
+        const doUpdate = (Date.now() - prefs['last-update']) / 1000 / 60 / 60 / 24 > 45;
+        if (doUpdate && previousVersion !== version) {
+          chrome.tabs.create({
+            url: page + '?version=' + version +
+              (previousVersion ? '&p=' + previousVersion : '') +
+              '&type=' + reason,
+            active: reason === 'install'
+          });
+          chrome.storage.local.set({'last-update': Date.now()});
+        }
+      }
+    });
+  });
+  setUninstallURL(page + '?rd=feedback&name=' + encodeURIComponent(name) + '&version=' + version);
+}
+
